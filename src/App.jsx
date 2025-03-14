143a302ff7fd6fefdf4c05152a2beb45273714ee
Eric Graham <wiltzie.eg@gmail.com>
2025-03-14 14:59:04 -0400

    made changes to app.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { useState } from 'react'
 import {puppyList} from "./data.js";
+import './App.css'
+import './index.css'
 
 function App() {
 
-const [puppies, setPuppies] = useState(puppyList);
-console.log(puppies);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
 
   return (
     <>
-      <div>
-
-      </div>
       <h1>Puppy Pals</h1>
       <div className="card">
         {
           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
+            return <p onClick={ ()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+        {featPupId && (
+          <div className='bottomDiv'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
